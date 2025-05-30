e69a31c79992d902a9cb2f50dbf2c95ea9dfa31b
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-30 08:33:07 -0500

    modify el user value

--- src/domain/entities/value-objects.ts
+++ src/domain/entities/value-objects.ts
@@ -1,19 +1,3 @@
-export class UserId {
-  constructor(private readonly value: string) {
-    if (!value || value.trim().length === 0) {
-      throw new Error('UserId cannot be empty');
-    }
-  }
-
-  getValue(): string {
-    return this.value;
-  }
-
-  equals(other: UserId): boolean {
-    return this.value === other.value;
-  }
-}
-
 export class Email {
   constructor(private readonly value: string) {
     if (!this.isValidEmail(value)) {
